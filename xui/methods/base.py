ca2c492ef4180fb67b6faaac6f6d4f347a7d147c
Arvin Radmehr <74110031+staliox@users.noreply.github.com>
2023-04-24 21:33:34 +0330

    Update base.py

--- xui/methods/base.py
+++ xui/methods/base.py
@@ -8,7 +8,7 @@ class Base:
     def _panel_address(self: "xui.XUI") -> str:
         return f"{self.https}://{self.address}:{self.port}{self.path}/"
 
-    def request(self: "xui.XUI", query: str, params: Union[dict, bool] = False) -> requests.Response:
+    def request(self: "xui.XUI", query: str, params: Union[dict, bool] = None) -> requests.Response:
         if query == "login":
             url = self._panel_address + query
         else:
@@ -20,8 +20,8 @@ class Base:
             cookie = None
 
         if not params:
-            response = requests.get(url, cookies=cookie, verify=False)
+            response = requests.get(url, cookies=cookie)
         else:
-            response = requests.post(url, cookies=cookie, data=params, verify=False)
+            response = requests.post(url, cookies=cookie, data=params)
 
         return response
