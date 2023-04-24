86c1c6e6686a8e89d058c312c2c3d1692c901148
Arvin Radmehr <74110031+staliox@users.noreply.github.com>
2023-04-24 21:33:11 +0330

    Update inbounds.py
    change condition

--- xui/methods/inbounds.py
+++ xui/methods/inbounds.py
@@ -5,7 +5,7 @@ class Inbounds:
     def get_inbounds(self: "xui.XUI") -> dict:
         _send_request = self.request("list")
 
-        if _send_request.status_code == 404:
+        if _send_request.status_code != 404 and _send_request.headers.get('Content-Type').startswith('application/json'):
+            return _send_request.json()
+        else:
             raise errors.NotFound()
-
-        return _send_request.json()
