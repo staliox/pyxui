3c5a0c91d28d9de6b5919a3391055ae670ccfbe7
Arvin Radmehr <74110031+staliox@users.noreply.github.com>
2023-04-24 21:45:56 +0330

    Update __init__.py
    fix vless generator

--- xui/config_gen/__init__.py
+++ xui/config_gen/__init__.py
@@ -2,11 +2,11 @@ import json
 import base64
 from urllib.parse import urlencode
 
-def config_generator(protocol: str, config = dict, **kwargs) -> str:
+def config_generator(protocol: str, config = dict, data: dict = None) -> str:
     if protocol == "vmess":
         string = "vmess://" + base64.b64encode(json.dumps(config).encode('utf-8')).decode('utf-8')
 
     elif protocol == "vless":
-        string = "vless://{}@{}:{}?{}#{}".format(info['id'], info['add'], info['port'], urlencode(config), info['ps'])
+        string = "vless://{}@{}:{}?{}#{}".format(config['id'], config['add'], config['port'], urlencode(data), config['ps'])
 
     return string
