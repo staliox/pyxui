5fe10ad60f531bcf2f4dc01e9f13b738ef57efed
Arvin Radmehr <74110031+staliox@users.noreply.github.com>
2023-04-25 15:34:41 +0330

    Update login.py
    rename library

--- pyxui/methods/login.py
+++ pyxui/methods/login.py
@@ -1,11 +1,11 @@
 from typing import Any
 
-import xui
-from xui import errors
+import pyxui
+from pyxui import errors
 
 class Login:
     def login(
-        self: "xui.XUI",
+        self: "pyxui.XUI",
         username: str,
         password: str
     ) -> Any:
@@ -38,11 +38,9 @@ class Login:
         if send_request.status_code == 200:
             self.session_string = send_request.cookies.get("session")
 
-=======
         if _send_request.status_code == 200:
             self.session_string = _send_request.cookies.get("session")
 
->>>>>>> 3c5a0c91d28d9de6b5919a3391055ae670ccfbe7
             if self.session_string:
                 return True
             else:
